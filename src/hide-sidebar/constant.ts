60f4ef453937b1f84e59159b0d21b8962038846a
xiaoboost <xiaoboost@qq.com>
2022-01-02 20:07:56 +0800

    fix: 选项放置至脚本选项处

--- src/hide-sidebar/constant.ts
+++ src/hide-sidebar/constant.ts
@@ -1,5 +1,3 @@
-import { ZhihuSelectorName } from 'src/utils/constant';
-
 /** 工具定义选择器 */
 export const SelectorName = {
   SideBarBtn: 'SideBar-Button',
@@ -11,6 +9,9 @@ export const SelectorName = {
   WidthFullMain: 'Zhihu-Width-Full',
 };
 
+/** 状态储存 */
+export const StoreKey = 'hide-sidebar';
+
 /** 状态枚举 */
 export enum Status {
   /** 默认状态 */
@@ -19,21 +20,8 @@ export enum Status {
   Hide,
 }
 
-/** 状态储存 */
-export const StoreKey = 'hide-sidebar';
-
-// eslint-disable-next-line
-export const defaultSvgPath = 'M408 442h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8zm-8 204c0 4.4 3.6 8 8 8h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56zm504-486H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 632H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM142.4 642.1L298.7 519a8.84 8.84 0 000-13.9L142.4 381.9c-5.8-4.6-14.4-.5-14.4 6.9v246.3a8.9 8.9 0 0014.4 7z';
-
-// eslint-disable-next-line
-export const hideSvgPath = 'M408 442h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8zm-8 204c0 4.4 3.6 8 8 8h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56zm504-486H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 632H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM115.4 518.9L271.7 642c5.8 4.6 14.4.5 14.4-6.9V388.9c0-7.4-8.5-11.5-14.4-6.9L115.4 505.1a8.74 8.74 0 000 13.8z';
-
-export const containers = [
-  `.${ZhihuSelectorName.MainContainer}`,
-  `.${ZhihuSelectorName.QuestionContainer}`,
-];
-
-export const menuName = {
-  Hide: '',
-  Default: '',
+/** 命令名称 */
+export const CommandName = {
+  [Status.Default]: '隐藏侧边栏',
+  [Status.Hide]: '恢复侧边栏',
 };
