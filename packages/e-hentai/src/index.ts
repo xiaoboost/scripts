3094f4a128fde89306d8e58f8f4bc0a07d1f46b6
xiaoboost <xiaoboost@qq.com>
2022-01-15 00:05:55 +0800

    fix: 移动组件代码，降低代码层级

--- packages/e-hentai/src/index.ts
+++ packages/e-hentai/src/index.ts
@@ -1,3 +1,23 @@
-import { active } from './render';
+import { MainButton } from './components/download-button';
+import { renderToDom, IdName } from 'src/utils';
+import { log } from '@scripts/utils';
 
-active();
+export function active() {
+  unsafeWindow.addEventListener('load', () => {
+    const rightAside = document.querySelector(`#${IdName.RightAside}`);
+
+    if (!rightAside) {
+      if (GlobalEnv.node === 'development') {
+        log('未发现侧边栏，下载按钮加载失败');
+      }
+
+      return;
+    }
+
+    rightAside.appendChild(renderToDom(MainButton));
+
+    if (GlobalEnv.node === 'development') {
+      log('下载按钮加载成功');
+    }
+  });
+}
